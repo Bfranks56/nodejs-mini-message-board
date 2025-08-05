12541c70469854bf987528ec1fe2cb79ea858fdd
bfranks56 <bryant.franks@gmail.com>
2025-08-05 10:34:23 -0400

    setup dotenv properly

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 // set up routes later
+
+
 const indexRouter = require("./routes/indexRouter")
 const path = require("node:path");
 
+dotenv.config();
 
 
 app.listen(process.env.PORT, () => {
