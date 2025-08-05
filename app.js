d0b4cc8f96e5b9af09610f288039e5b043bddd99
bfranks56 <bryant.franks@gmail.com>
2025-08-05 11:09:59 -0400

    begin creating newMessageRouter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/indexRouter');
+const newMessageRouter = require('./routes/newRouter');
 const path = require('node:path');
 
 dotenv.config();
@@ -10,6 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Set up routes
+app.use('/new', newMessageRouter);
 app.use('/', indexRouter);
 
 // Error handling middleware
