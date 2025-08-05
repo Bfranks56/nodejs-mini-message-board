00bd3332ed212c1490498ab5327e3f3adab88d93
bfranks56 <bryant.franks@gmail.com>
2025-08-05 10:41:45 -0400

    set up index route properly

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const dotenv = require("dotenv");
-const express = require("express");
+const dotenv = require('dotenv');
+const express = require('express');
 const app = express();
-// set up routes later
-
-
-const indexRouter = require("./routes/indexRouter")
-const path = require("node:path");
+const indexRouter = require('./routes/indexRouter');
+const path = require('node:path');
 
 dotenv.config();
 
+app.use('/', indexRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
 
 app.listen(process.env.PORT, () => {
-    console.log('Server is running on port ' + process.env.PORT);
+  console.log('Server is running on port ' + process.env.PORT);
 });
